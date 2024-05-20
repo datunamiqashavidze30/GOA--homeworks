35f76755d108704037c07aea27a302b025c6bd22
datunamiqashavidze30 <datunamikashavidze@gmail.com>
2024-05-20 23:23:16 +0400

    first commit

---
+++ Level 01/homework/lesson1.py
@@ -0,0 +1,7 @@
+name = "dato"
+surname = "miqashavidze"
+age = 10
+height = 136
+knows_programming = True
+print(name + " " + str(age))
+print(name +  " " + str(surname) + " " + str(age) + " " + str(height) + " " + str( knows_programming))
