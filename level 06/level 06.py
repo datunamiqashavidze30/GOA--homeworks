909dabc3bfc25b353acfa77615ec60878c73bdc5
datunamiqashavidze30 <datunamikashavidze@gmail.com>
2024-06-13 18:57:22 +0400

    third commit

---
+++ level 06/level 06.py
@@ -0,0 +1,7 @@
+# შევქვნათ ცვლადები
+name = "data"
+surname = "diasamidze"
+age = 100
+text = "I am programmer"
+# დავპრინტოთ ცვლადები არითმეტიკული მოქმედებებით
+print("My name is" + " " + name + " " + "my surname is" + " " + surname + " " + "my age is" + " " + str(age) + " " + text)
